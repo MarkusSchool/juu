89c7e82fd5fa2daf2ac0845f6c601916fe94aaa7
MarkusSchool <nokelainennmarkus@gmail.com>
2023-02-14 11:09:02 +0200

    Tehtiin demo7 kirjautuminen

---
+++ demo7/kirjautuminen.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut() {
+    let kirjautunut = "kyllä";
+    if(kirjautunut === "kyllä") {
+        document.getElementById("tervetulo_teksti").textContent += " " + localStorage.getItem("nimi");
+    }
+}
+
+function kirjaudu() {
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+
+}
